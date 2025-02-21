90ddff0a70173d2a9386651c3a02df7e78c3c3ed
RShnitser <romanshnitser@hotmail.com>
2025-02-21 09:49:11 -0500

    add publish json

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+    amqp "github.com/rabbitmq/amqp091-go"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+)
+
 
 func main() {
+	connectionString := "amqp://guest:guest@localhost:5672/"
+	connection, err := amqp.Dial(connectionString)
+	if(err != nil){
+		return
+	}
+
+	defer connection.Close()
+
+	channel, err := connection.Channel()
+	if(err != nil){
+		return
+	}
+
+	err = pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{true})
+
 	fmt.Println("Starting Peril server...")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
+	fmt.Println("Shutting Down")
 }
